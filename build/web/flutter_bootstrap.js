7dfd83a733fd92c335b81edefd0e78fc6298d490
MMNU <mmnu@example.com>
2026-05-22 22:17:16 +0900

    Force overwrite everything with umami code

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3074629553" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3294808760" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
